4a7fcf7f4f65edaccafefd96bfd6242520d92464
Federico Sedia <federicosdia@gmail.com>
2023-05-18 21:08:47 +0200

    inkwell

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:meals_app/main.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
@@ -9,25 +10,33 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //padding -> contorno al widget
-      padding: const EdgeInsets.all(16),
-      //variazione colore in opacità
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          category.color.withOpacity(0.55),
-          category.color.withOpacity(0.45),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      ),
-      child: Text(
-        category.title,
-        // ! -> definitivo
-        //copywith copio il testo predefinito
-        //colore sempre preso dal tema e con colore di sfondo
-        style: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    //inkwell a differenza di gesturedetector
+    //gestisce anche gli effetti visivi
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        //padding -> contorno al widget
+        padding: const EdgeInsets.all(16),
+        //variazione colore in opacitàg
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.45),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: Text(
+          category.title,
+          // ! -> definitivo
+          //copywith copio il testo predefinito
+          //colore sempre preso dal tema e con colore di sfondo
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
       ),
     );
   }
